923fd72ed43076134f75828d5d2be6536e935df8
Jesse <jesse.lee.smith@gmail.com>
2020-03-11 19:33:50 -0700

    customizing controllers

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -1,2 +1,23 @@
 class PlayersController < ApplicationController
+  respond_to :json
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
