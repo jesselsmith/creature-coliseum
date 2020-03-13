75430bd2b0c021c47858512181e3fbdad73c72be
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-13 15:55:26 -0700

    got xp and adjusted xp working

--- backend/app/models/encounter.rb
+++ backend/app/models/encounter.rb
@@ -3,8 +3,22 @@ class Encounter < ApplicationRecord
   has_many :players
   has_many :monsters
 
-  def xp()
+  def xp
     self.monsters.sum(&:xp)
+  end
+
+  def adjusted_xp
+    adjustment_table = [0.5, 1, 1.5, 2, 2.5, 3, 4, 5]
+    table_index = 1
+    if self.players.size <= 3
+      table_index += 1
+    elsif self.players.size >= 6
+      table_index -= 1
+    end
+    buckets = [14, 10, 6, 2, 1, 0]
+    table_index += 5 - buckets.index { |bucket| self.monsters.size > bucket }
+    
+    self.xp * adjustment_table[table_index]
   end
 
 end
--- backend/app/models/monster.rb
+++ backend/app/models/monster.rb
@@ -2,75 +2,20 @@ class Monster < ApplicationRecord
   belongs_to :encounter
 
   def xp()
-    case self.cr
-    when '0'
-      10
-    when '1/8'
-      25
-    when '1/4'
-      50
-    when '1/2'
-      100
-    when '1'
-      200
-    when '2'
-      450
-    when '3'
-      700
-    when '4'
-      1100
-    when '5'
-      1800
-    when '6'
-      2300
-    when '7'
-      2900
-    when '8'
-      3900
-    when '9'
-      5000
-    when '10'
-      5900
-    when '11'
-      7200
-    when '12'
-      8400
-    when '13'
-      10_000
-    when '14'
-      11_500
-    when '15'
-      13_000
-    when '16'
-      15_000
-    when '17'
-      18_000
-    when '18'
-      20_000
-    when '19'
-      22_000
-    when '20'
-      25_000
-    when '21'
-      33_000
-    when '22'
-      41_000
-    when '23'
-      50_000
-    when '24'
-      62_000
-    when '25'
-      75_000
-    when '26'
-      90_000
-    when '27'
-      105_000
-    when '28'
-      120_000
-    when '29'
-      135_000
-    when '30'
-      155_000
+    xp_table = [10, 25, 50, 100, 200, 450, 700, 1100, 1800, 2300, 2900, 3900,
+      5000, 5900, 7200, 8400, 10_000, 11_500, 13_000, 15_000, 18_000, 20_000,
+      22_000, 25_000, 33_000, 41_000, 50_000, 62_000, 75_000, 90_000, 105_000,
+      120_000, 135_000, 155_000]
+    if self.cr == '0'
+      xp_table[0]
+    elsif self.cr == '1/8'
+      xp_table[1]
+    elsif self.cr == '1/4'
+      xp_table[2]
+    elsif self.cr == '1/2'
+      xp_table[3]
+    elsif (1..30).include?(self.cr.to_i)
+      xp_table[self.cr.to_i + 3]
     else
       0
     end
