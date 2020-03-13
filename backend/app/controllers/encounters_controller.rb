0064f994aec80faf7d32583c0228d6b4a73755eb
Jesse <jesse.lee.smith@gmail.com>
2020-03-13 13:46:26 -0700

    updating backend

--- backend/app/controllers/encounters_controller.rb
+++ backend/app/controllers/encounters_controller.rb
@@ -6,24 +6,43 @@ class EncountersController < ApplicationController
   end
 
   def create
-
+    encounter = Encounter.new(encounter_params(params))
+    if encounter.save
+      render json: EncounterSerializer.new(encounter)
+    else
+      render json: { message: 'There was an error in encounter creation' }
+    end
   end
 
   def show
-
+    render_encounter
   end
 
   def update
-
+    render_encounter { @encounter.update(encounter_params(params)) }
   end
 
   def destroy
-
+    render_encounter { @encounter.destroy }
   end
 
   private
 
-  def new_encounter_params(params)
+  def encounter_params(params)
     params.require(:encounter).permit(:title)
+  end
+
+  def render_encounter
+    @encounter = find_encounter
+    if @encounter
+      yield
+      render json: EncounterSerializer.new(@encounter)
+    else
+      render json: { message: 'That encounter could not be found' }
+    end
+  end
+
+  def find_encounter
+    encounter.find_by(id: params[:id])
   end
 end
--- backend/app/controllers/monsters_controller.rb
+++ backend/app/controllers/monsters_controller.rb
@@ -2,22 +2,47 @@ class MonstersController < ApplicationController
   respond_to :json
 
   def index
-
+    render json: MonsterSerializer.new(Monster.all)
   end
 
   def create
-
+    monster = Monster.new(Monster_params(params))
+    if monster.save
+      render json: MonsterSerializer.new(Monster)
+    else
+      render json: { message: 'There was an error in monster creation' }
+    end
   end
 
   def show
-
+    render_monster
   end
 
   def update
-
+    render_monster { @monster.update(monster_params(params)) }
   end
 
   def destroy
+    render_monster { @monster.destroy }
+  end
 
+  private
+
+  def Monster_params(params)
+    params.require(:monster).permit(:name, :initiative_bonus, :cr, :url)
+  end
+
+  def render_monster
+    @monster = find_monster
+    if @monster
+      yield
+      render json: MonsterSerializer.new(@monster)
+    else
+      render json: { message: 'That monster could not be found' }
+    end
+  end
+
+  def find_monster
+    Monster.find_by(id: params[:id])
   end
 end
