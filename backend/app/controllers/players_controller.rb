7b5655c4ff56d88020cb6030d847d5a2e7c85401
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-13 14:34:59 -0700

    added detail to players controller

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -2,22 +2,47 @@ class PlayersController < ApplicationController
   respond_to :json
 
   def index
-
+    render json: PlayerSerializer.new(Player.all)
   end
 
   def create
-
+    player = Player.new(player_params(params))
+    if player.save
+      render json: PlayerSerializer.new(player)
+    else
+      render json: { message: 'There was an error in player creation' }
+    end
   end
 
   def show
-
+    render_player
   end
 
   def update
-
+    render_player { @player.update(player_params(params)) }
   end
 
   def destroy
+    render_player { @player.destroy }
+  end
 
+  private
+
+  def player_params(params)
+    params.require(:player).permit(:name, :initiative_bonus, :cr, :url)
+  end
+
+  def render_player
+    @player = find_player
+    if @player
+      yield
+      render json: PlayerSerializer.new(@player)
+    else
+      render json: { message: 'That player could not be found' }
+    end
+  end
+
+  def find_player
+    Player.find_by(id: params[:id])
   end
 end
