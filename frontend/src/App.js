1a9e8782253fe066e3f552a6e61e320d894f32b3
Jesse <jesse.lee.smith@gmail.com>
2020-03-14 16:17:34 -0700

    Continuing to add files to the frontend and working on beginnings of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import EncounterList from './containers/EncounterList'
 
-function App() {
-	return ( 
-			<div>
-				ENCOUNTERS 
-			</div>
-	);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ENCOUNTERS</h1>
+        <EncounterList />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/reducers/encountersReducer.js
@@ -0,0 +1,8 @@
+const encountersReducer = (state = { encounters: [], monsters: [], players: [] }, action) => {
+  switch (action.type) {
+    case 'ADD_ENCOUNTER':
+      return state
+    default:
+      return state
+  }
+}
