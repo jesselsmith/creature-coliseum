4785dae83603609cc7455a101c9604dc9e30a5a0
Jesse <jesse.lee.smith@gmail.com>
2020-03-15 01:41:33 -0700

    fixed bug where I never changed the loading flag back to false, so the components thought it was always loading

--- frontend/src/reducers/encountersReducer.js
+++ frontend/src/reducers/encountersReducer.js
@@ -1,9 +1,9 @@
 export default (state = { encounters: [], loading: false }, action) => {
   switch (action.type) {
     case 'ADD_ENCOUNTERS':
-      return { ...state, encounters: [...state.encounters, ...action.encounters] }
+      return { ...state, encounters: [...state.encounters, ...action.encounters], loading: false }
     case 'ADD_ENCOUNTER':
-      return { ...state, encounters: [...state.encounters, action.encounter] }
+      return { ...state, encounters: [...state.encounters, action.encounter], loading: false }
     case 'LOADING_ENCOUNTERS':
       return {
         ...state,
