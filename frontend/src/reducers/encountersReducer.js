7590f45a93c325230fcf2e6bfd07805c20da92b7
Jesse <jesse.lee.smith@gmail.com>
2020-03-16 16:25:29 -0700

    adding other actions to reducer. still needs refactoring

--- frontend/src/reducers/encountersReducer.js
+++ frontend/src/reducers/encountersReducer.js
@@ -7,24 +7,21 @@ export default (state = { encounters: [], monsters: [], players: [], loading: fa
         players: action.players,
         loading: false
       }
-    case 'ADD_ENCOUNTERS':
-      return {
-        ...state,
-        encounters: [...state.encounters, ...action.encounters],
-        monsters: [...state.monsters, ...action.monsters],
-        players: [...state.players, ...action.players],
-        loading: false
-      }
     case 'ADD_ENCOUNTER':
       return { ...state, encounters: [...state.encounters, action.encounter], loading: false }
     case 'LOADING_ENCOUNTERS':
       return {
         ...state,
-        encounters: [...state.encounters],
         loading: true
       }
     case 'ADD_PLAYER':
       return { ...state, players: [...state.players, action.player], loading: false }
+    case 'REMOVE_PLAYER':
+      return { ...state, players: state.players.filter(player => player.id !== action.playerId), loading: false }
+    case 'ADD_MONSTER':
+      return { ...state, monsters: [...state.monsters, action.monster], loading: false }
+    case 'REMOVE_MONSTER':
+      return { ...state, monsters: state.monsters.filter(monster => monster.id !== action.monsterId), loading: false }
     default:
       return state
   }
