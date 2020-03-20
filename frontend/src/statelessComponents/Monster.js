83bdec7790039899a71d226dd85d68c66df5356f
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-20 10:41:26 -0700

    added delete button for monsters, functional!

--- frontend/src/statelessComponents/Monster.js
+++ frontend/src/statelessComponents/Monster.js
@@ -4,6 +4,7 @@ const monster = props => {
   return (
     <div>
       {props.monster.attributes.name}, CR: { props.monster.attributes.cr}
+      <button onClick={() => props.deleteMonster(props.monster.id)} >X</button>
     </div >
   )
 }
