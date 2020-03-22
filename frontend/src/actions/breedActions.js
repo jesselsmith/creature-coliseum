94dca3db3592d8747b8a974e2a7e91f5cfa82ad2
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-22 16:09:26 -0700

    fixed up fetchbreeds action

--- frontend/src/actions/breedActions.js
+++ frontend/src/actions/breedActions.js
@@ -1,16 +1,22 @@
 const BREED_URL = 'http://localhost:3001/breeds'
 
+
 const OPTIONS = {
 
 }
 
+const queryString = require('querystring')
+
 
 export const fetchBreeds = (searchHash = null) => {
+  debugger
+  let url = BREED_URL
+  if(searchHash !== null){
+    url += '?' + queryString(searchHash)
+  }
   return dispatch => {
-    url = BREED_URL
-    if(searchHash !== null){
-      
-    }
-    fetch(BREED_URL)
+    dispatch({ type: 'LOADING_BREEDS' })
+    fetch(url).then(resp => resp.json())
+    .then(json => dispatch({type: 'SET_BREEDS', breeds: json.data}))
   }
 }
