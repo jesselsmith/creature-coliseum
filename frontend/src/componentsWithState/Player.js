12fd34050057a3fd4ab03f2630c256beedc71d69
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-23 16:18:08 -0700

    modified player component for ability to edit and away from functional component;

--- frontend/src/componentsWithState/Player.js
+++ frontend/src/componentsWithState/Player.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const player = props => {
-  return (
-    <div>
-      {props.player.attributes.name}, Level: { props.player.attributes.level}
-      <button onClick={() => props.deletePlayer(props.player.id)} >X</button>
-    </div >
-  )
+
+class Player extends Component{
+  state = {
+    showForm: false
+  }
+
+  displayInfoOrForm = () => {
+    if(this.state.showForm){
+      return <PlayerForm
+        encounterId={this.props.player.relationships.encounter.id}
+        method={'PATCH'}
+        unmount={() => this.setState({showForm: false})}
+        currentState={{
+          name: this.props.player.attributes.name,
+          level: this.props.player.attributes.level,
+          encounter_id: this.props.player.relationships.encounter.id
+        }}
+        playerId = {this.props.player.id}
+      />
+    }else{
+      return (
+        <div>
+          {this.props.player.attributes.name}, Level: { this.props.player.attributes.level}
+          <EditButton edit={() =>{this.setState({showForm: true})}} />
+          <button onClick={() => this.props.deletePlayer(this.props.player.id)} >X</button>
+        </div >
+      ) 
+    }
+  }
+
+  render(){
+    return this.displayInfoOrForm()
+  }
 }
 
-export default player
+export default Player
