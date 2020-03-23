f33bf41c84620d2a1f5a6d294b48b8664e5a0303
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-23 00:45:15 -0700

    implemented event handling for breedsearch

--- frontend/src/componentsWithState/BreedSearch.js
+++ frontend/src/componentsWithState/BreedSearch.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchBreeds } from '../actions/breedActions'
 
 class BreedSearch extends Component{
 
@@ -29,28 +31,37 @@ class BreedSearch extends Component{
 
   state = {
     name: '',
-    monster_type: 'Any Type',
+    monster_type: '',
     spellcaster: '',
-    size_category: 'Any Size',
-    min_cr: 'Min CR',
-    max_cr: 'Max CR',
-    min_ac: 'Min AC',
-    max_ac: 'Max AC',
-    min_attack_bonus: 'Min Attack Bonus',
-    max_attack_bonus: 'Max Attack Bonus'
+    size_category: '',
+    min_cr: '',
+    max_cr: '',
+    min_ac: '',
+    max_ac: '',
+    min_attack_bonus: '',
+    max_attack_bonus: ''
   }
 
   makeOptions = array => {
     return array.map(num => <option value={num} key={num} >{num}</option>)
   }
 
+  handleOnSubmit = e => {
+    e.preventDefault()
+    this.props.fetchBreeds(this.state)
+  }
+
+  handleChange = e => {
+    this.setState(prev => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
   render(){
     return (
       <form onSubmit={this.handleOnSubmit}>
         <div>
-          <input type='text' name='name' value={this.state.name} onChange={this.handleOnChange} placeholder='Name...'/>
-          <select name='monster_type' value={this.state.monster_type} onChange={this.handleOnChange} >
-            <option value='Any Type'>Any Type</option>
+          <input type='text' name='name' value={this.state.name} onChange={this.handleChange} placeholder='Name...'/>
+          <select name='monster_type' value={this.state.monster_type} onChange={this.handleChange} >
+            <option value=''>Any Type</option>
             <option value='Aberration'>Aberration</option>
             <option value='Beast'>Beast</option>
             <option value='Celestial'>Celestial</option>
@@ -67,7 +78,7 @@ class BreedSearch extends Component{
             <option value='Undead'>Undead</option>
           </select>
           <select name='size_category' value={this.state.size_category} onChange={this.handleChange}>
-            <option value='Any Size'>Any Size</option>
+            <option value=''>Any Size</option>
             <option value="tiny">Tiny</option>
             <option value="small">Small</option>
             <option value="medium">Medium</option>
@@ -82,35 +93,36 @@ class BreedSearch extends Component{
             <option value="false">false</option>
           </select>
           <select name='min_cr' value={this.state.min_cr} onChange={this.handleChange}>
-            <option value='Min CR'>Min CR</option>
+            <option value=''>Min CR</option>
             {this.makeOptions(this.CR_ARRAY)}
           </select>
           <select name='max_cr' value={this.state.max_cr} onChange={this.handleChange}>
-            <option value='Max CR'>Max CR</option>
+            <option value=''>Max CR</option>
             {this.makeOptions(this.CR_ARRAY)}
           </select>
         </div>
         <div>
           <select name='min_ac' value={this.state.min_ac} onChange={this.handleChange}>
-            <option value='Min AC'>Min AC</option>
+            <option value=''>Min AC</option>
             {this.makeOptions(this.AC_ARRAY)}
           </select>
           <select name='max_ac' value={this.state.max_ac} onChange={this.handleChange}>
-            <option value='Max AC'>Max AC</option>
+            <option value=''>Max AC</option>
             {this.makeOptions(this.AC_ARRAY)}
           </select>
           <select name='min_attack_bonus' value={this.state.min_attack_bonus} onChange={this.handleChange}>
-            <option value='Min Attack Bonus'>Min Attack Bonus</option>
+            <option value=''>Min Attack Bonus</option>
             {this.makeOptions(this.ATTACK_BONUS_ARRAY)}
           </select>
           <select name='max_attack_bonus' value={this.state.max_attack_bonus} onChange={this.handleChange}>
-            <option value='Max Attack Bonus'>Max Attack Bonus</option>
+            <option value=''>Max Attack Bonus</option>
             {this.makeOptions(this.ATTACK_BONUS_ARRAY)}
           </select>
+          <input type='submit' value='Filter Monsters' />
         </div>
       </form>
     )
   }
 }
 
-export default BreedSearch
+export default connect(null, { fetchBreeds })(BreedSearch)
