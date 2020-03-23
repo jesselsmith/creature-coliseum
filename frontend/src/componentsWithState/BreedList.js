84cdb4d87ba0b75dd53c9407c849e63a0258cbfa
jesselsmith <jesse.lee.smith@gmail.com>
2020-03-23 02:10:52 -0700

    adding buttons to navigate through breed search results

--- frontend/src/componentsWithState/BreedList.js
+++ frontend/src/componentsWithState/BreedList.js
@@ -6,9 +6,11 @@ import { postMonster } from '../actions/fetchActions'
 
 class BreedList extends Component {
 
+  PAGE_LENGTH = 25
+
   state = {
     currentPage: 1,
-    numPage: this.props.breeds.length / 50 + 1
+    numPage: this.props.breeds.length / this.PAGE_LENGTH + 1
   }
 
   addMonsterFromBreed = breedId => {
@@ -19,11 +21,29 @@ class BreedList extends Component {
     if(this.props.loading){
       return <tr><td>Monsters Loading...</td></tr>
     }else{
-      const index = this.state.currentPage * 50 - 50
-      return this.props.breeds.slice(index, index + 50).map(breed => {
+      const index = this.state.currentPage * this.PAGE_LENGTH - this.PAGE_LENGTH
+      return this.props.breeds.slice(index, index + this.PAGE_LENGTH).map(breed => {
         return <Breed key={breed.id} breed={breed} addMonster={this.addMonsterFromBreed} />
       })
     }
+  }
+
+  displayPageButtons = () => {
+    return(
+      <div>
+        {(() => {
+          if(this.state.numPage > 1){
+            return <span><button>Page 1</button><button>Previous</button></span>
+          }
+        })()}
+        <button>Page {this.currentPage}</button>
+        {(() =>{
+          if(this.currentPage !== this.state.numPage){
+            return <span><button>Next</button><button>Page {this.state.numPage}</button></span>
+          }
+        })()}
+      </div>
+    )
   }
 
   render(){
@@ -47,6 +67,7 @@ class BreedList extends Component {
             {this.displayBreeds()}
           </tbody>
         </table>
+        {this.displayPageButtons()}
       </div>
       
     )
